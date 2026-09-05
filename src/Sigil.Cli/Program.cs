ed39aef37f20136b46bb04e790cc50859e7eb291
Jake Hathaway <jake.d.hathaway@gmail.com>
2026-09-04 23:43:18 -0700

    add simple cli compiler

--- src/Sigil.Cli/Program.cs
+++ src/Sigil.Cli/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Sigil.Compiler;
+
+if (args.Length != 3 || args[1] != "-o")
+{
+    Console.Error.WriteLine(
+        "Usage: sigil <source.sgl> -o <output>");
+
+    return 1;
+}
+
+var sourcePath = args[0];
+var outputPath = args[2];
+
+if (!File.Exists(sourcePath))
+{
+    Console.Error.WriteLine(
+        $"Source file not found: {sourcePath}");
+
+    return 1;
+}
+
+try
+{
+    var source = File.ReadAllText(sourcePath);
+
+    new Compiler().Compile(
+        source,
+        outputPath);
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine(ex.Message);
+
+    return 1;
+}
